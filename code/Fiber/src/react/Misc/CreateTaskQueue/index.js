417ef854ddffad2a4d496af151109200ef0677d9
cinyearchan <cinyearchan@gmail.com>
2021-01-12 17:27:16 +0800

    createTaskQueue添加注释

--- code/Fiber/src/react/Misc/CreateTaskQueue/index.js
+++ code/Fiber/src/react/Misc/CreateTaskQueue/index.js
@@ -1,7 +1,9 @@
 const createTaskQueue = () => {
   const taskQueue = []
   return {
+    // 向任务队列中添加任务
     push: item => taskQueue.push(item),
+    // 从任务队列中获取任务
     pop: () => taskQueue.shift()
   }
 }
