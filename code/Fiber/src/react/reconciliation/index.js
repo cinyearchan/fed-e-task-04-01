6236ef03de220c73bcb5b657dd2853c5d009087c
cinyearchan <cinyearchan@gmail.com>
2021-01-14 15:52:22 +0800

    构建根节点 fiber 对象

--- code/Fiber/src/react/reconciliation/index.js
+++ code/Fiber/src/react/reconciliation/index.js
@@ -4,7 +4,19 @@ const taskQueue = createTaskQueue()
 
 const subTask = null
 
-const getFirstTask = () => {}
+const getFirstTask = () => {
+  // 从任务队列中获取任务
+  const task = taskQueue.pop()
+
+  // 返回最外层节点的 fiber 对象
+  return {
+    props: task.props,
+    stateNode: task.dom,
+    tag: "host_root",
+    effects: [],
+    child: null
+  }
+}
 
 const executeTask = fiber => {}
 
